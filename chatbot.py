30ceccc72c46af23516faceaf60cc7887f39c734
Sumanthrapolu <110659029+Sumanthrapolu@users.noreply.github.com>
2022-10-31 20:18:43 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,62 @@
+from re import M
+
+
+print("hello\n")
+
+
+print("who are you\n")
+print("Can I interact with you for some time\n")
+M=input(    )
+print("\n how are you  \n")
+print("1.fine\n")
+print("2.not fine\n")
+n=int(input(  ))
+if (n==1) :
+    print("It feels good\n")
+elif (n==2) :
+    print("why.... whats wrong???\n")
+    print("Is every thing okey\n")
+    s=input()
+    print("DONT WORRY EVERY THING WILL BE ALRIGHT\n")
+print("okey then whats your age\n")
+u=int(input())
+if u>=18 and u<=23 :
+    print("\n so you must have known about engineering?\n")
+elif u>25 :
+    print("\n so you must have known about engineering")
+print("1.yes")
+print("2.no \n")
+a =int(input())
+if (a==1) :
+    print("\n thats great\n")
+elif (a==2) :
+    print("okey there is nothing wrong about it")
+    print("you just beleive in your self\n")
+print("have you sudied engineering or currently studying\n")
+print("1.completed engineering")
+print("2.studying engineering")
+print("3.not in the field of engineering\n")
+t=int(input(  ))
+if (t==1) :
+    print("which year you have completed your engineering\n")
+    h=int(input())
+    print("THATS REALLY GREAT\n")
+    print("So you would have a lot of knowledge in it\n")
+elif (t==2) :
+    print("what college are you studying in ???\n")
+    r=input()
+    print("\n IN WHAT YEAR U ARE IN \n")
+    print("***PLEASE ENTER IN WORDS***\n")
+    p=input()
+    print("\n what branch have you taken in engineering??\n")
+    f=input()
+    print("\n THAT'S REALLY A GOOD STREAM....\n")
+    print("***THATS REALLY A GREAT CHOICE***\n")
+elif (t==3) :
+    print("okey then... what are you actually doing now then???\n")
+    w=input() 
+    print("even yours is also a good course\n")
+print("okey it seems we have been intaracting for a long time\n ")           
+print("by this conversation I have known that U are a great person by beauty and heart\n") 
+print("thank you for having a great conversation with me and giving me a reason to be created\n")
+print("SEE YOU NEXT TIME\n")
